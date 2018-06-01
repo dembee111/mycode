59384c24a91b3bd8660a290318071ae577529a75
dembee111 <minion.ck@gmail.com>
2018-06-01 15:59:12 +0800

    complete filter crud

--- JQuery/multi_crud/index.php
+++ JQuery/multi_crud/index.php
@@ -36,14 +36,14 @@
 </div>
 <div id="user_dialog" title="Add Data">
   <div class="form-group">
-    <label>Enter First Name</label>
+    <label>Өөрийн нэрийг оруулна уу</label>
     <input type="text" name="first_name" id="first_name" class="form-control" />
     <span id="error_first_name" class="text-danger"></span>
    </div>
 
 
   <div class="form-group">
-    <label>Enter Last Name</label>
+    <label>Овог оруулна уу</label>
     <input type="text" name="last_name" id="last_name" class="form-control" />
     <span id="error_last_name" class="text-danger"></span>
    </div>
@@ -51,7 +51,7 @@
    <div class="form-group" align="center">
 
      <input type="hidden" name="row_id" value="hidden_row_id" />
-     <button type="button" name="save" id="save" class="btn btn-info">Save</button>
+     <button type="button" name="save" id="save" class="btn btn-info">Хадгалах</button>
 
     </div>
     </div>
@@ -70,7 +70,7 @@ $('#user_dialog').dialog({
   width:400
 });
 $('#add').click(function(){
-  $('#user_dialog').dialog('option','title','Add Data');
+  $('#user_dialog').dialog('option','title','Мэдээлэл хүснэгтэд нэмэх');
   $('#first_name').val('');
   $('#last_name').val('');
   $('#error_first_name').text('');
@@ -87,7 +87,7 @@ $('#save').click(function(){
      var last_name = '';
      if($('#first_name').val() =='')
      {
-       error_first_name = 'First Name is required';
+       error_first_name = 'Нэрээ оруулах шаардлагатай';
        $('#error_first_name').text(error_first_name);
        $('#first_name').css('border-color', '#cc0000');
        first_name= '';
@@ -97,9 +97,107 @@ $('#save').click(function(){
         error_first_name = '';
         $('#error_first_name').text(error_first_name);
         $('#first_name').css('border-color', '');
-        11:34 
-     }
-});
+        first_name = $('#first_name').val();
 
+
+     }
+     if($('#last_name').val()=='')
+     {
+        error_last_name = 'Овогоо оруулна уу';
+        $('#error_last_name').text(error_last_name);
+        $('#last_name').css('border-color', '#cc0000');
+        last_name = '';
+     }
+     else {
+       error_last_name = '';
+       $('#error_last_name').text(error_last_name);
+       $('#last_name').css('border-color', '');
+       last_name = $('#last_name').val();
+}
+if(error_first_name != '' || error_last_name != '')
+{
+  return false;
+}
+else
+{
+  if($('#save').text()=='Save')
+  {
+     count = count +1;
+     output = '<tr id="row_'+count+'">';
+     output += '<td>'+first_name+' <input type="hidden" name="hidden_first_name[]" id="first_name'+count+'" class="first_name" value="'+first_name+'"/></td>';
+     output += '<td>'+last_name+' <input type="hidden" name="hidden_last_name[]" id="last_name'+count+'" class="last_name" value="'+last_name+'"/></td>';
+     output += '<td><button type="button" name="view_details" class="btn btn-warinig btn-xs view_details" id="'+count+'">View</button></td>';
+     output += '<td><button type="button" name="remove_details" class="btn btn-danger btn-xs remove_details" id="'+count+'">Remove</button></td>';
+     output += '</tr>';
+
+
+     $('#user_data').append(output);
+  }
+  else {
+    var row_id = $('#hidden_row_id').val();
+    output += '<td>'+first_name+' <input type="hidden" name="hidden_first_name[]" id="first_name'+row_id+'" class="first_name" value="'+first_name+'"/></td>';
+    output += '<td>'+last_name+' <input type="hidden" name="hidden_last_name[]" id="last_name'+row_id+'" class="last_name" value="'+last_name+'"/></td>';
+    output += '<td><button type="button" name="view_details" class="btn btn-warinig btn-xs view_details" id="'+row_id+'">View</button></td>';
+    output += '<td><button type="button" name="remove_details" class="btn btn-danger btn-xs remove_details" id="'+row_id+'">Remove</button></td>';
+
+    $('#row_'+row_id+'').html(output);
+  }
+  $('#user_dialog').dialog('close');
+}
+});
+$(document).on('click', '.view_details', function(){
+  var row_id = $(this).attr("id");
+  var first_name = $('#first_name'+row_id+'').val();
+  var last_name = $('#last_name'+row_id+'').val();
+  $('#first_name').val(first_name);
+  $('#last_name').val(last_name);
+  $('#save').text('Edit');
+  $('#hidden_row_id').val(row_id);
+  $('#user_dialog').dialog('option','title','Edit Data');
+  $('#user_dialog').dialog('open');
+});
+$(document).on('click', '.remove_details', function(){
+  var row_id = $(this).attr("id");
+  if(confirm("Are you sure you want to remove this row data?"))
+  {
+    $('#row_'+row_id+'').remove();
+  }
+  else{
+    return false;
+  }
+
+});
+$('#action_alert').dialog({
+  autoOpen:false
+});
+$('#user_form').on('submit', function(event){
+  event
+  e.preventDefault();
+  var count_data = 0;
+  $('.first_name').each(function(){
+    count_data = count_data + 1;
+
+  });
+  if(count_data > 0)
+  {
+     var form_data = $(this).serialize();
+     $.ajax({
+       url:"insert.php",
+       method:"POST",
+       data:form_data,
+       success:function(data)
+       {
+         $('#user_data').find("tr:gt(0)").remove();
+         $('#action_alert').html('<p>Data Inserted Successfully</p>');
+         $('#action_alert').dialog('open');
+       }
+     })
+  }
+  else
+  {
+    $('#action_alert').html('<p>Please Add atleast one data</p>');
+    $('#action_alert').dialog('open');
+  }
+});
 });
 </script>
